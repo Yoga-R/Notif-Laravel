6be26bfa5784e0ff97dee61ed4dafa3537e5f6c6
yoga-r <yogariski27@gmail.com>
2025-01-31 14:47:51 +0700

    fix docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Jalankan migrasi database (opsional)
+# Jalankan migrasi database (uncomment jika ingin otomatis)
 # php artisan migrate --force
 
-# Start services
+# Generate APP_KEY jika belum ada
+if [ -z "$(grep APP_KEY=base64:.b3 .env)" ]; then
+    php artisan key:generate
+fi
+
+# Start Nginx dan PHP-FPM
+echo "Starting Nginx..."
 service nginx start
+
+echo "Starting PHP-FPM..."
 php-fpm
